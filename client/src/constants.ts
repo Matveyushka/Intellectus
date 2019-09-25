34593b4e89e30d067b83030dbffd4f231e989277
Andrew <Andrew5282011@gmail.com>
2019-09-25 22:08:05 +0300

    INT-5 Add main page

---
+++ client/src/constants.ts
@@ -0,0 +1,6 @@
+export const URLS = {
+  main: '/',
+  statistics: '/statistics',
+  about: '/about',
+  contactUs: '/contact-us',
+} as const;
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,1 +1,13 @@
-export const Main = (): React.ReactElement | null => null;
+import * as React from 'react';
+
+export const Main = (): React.ReactElement | null => (
+  <div className="main-container">
+    <h1 className="title">CHALLENGE</h1>
+    <h1 className="title">YOUR MIND</h1>
+    <div className="play-icon" />
+    <div className="footer">
+      <p>INTELLECTUS</p>
+      <p>BY NOBRAINS</p>
+    </div>
+  </div>
+);
