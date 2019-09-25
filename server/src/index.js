3ce687a8bc9c44b43218aaa0ad16e2c3a1d7fde1
STAJLb <kzhelnov19@yandex.ru>
2019-09-25 15:45:01 +0300

    Create server and first route (#5)
    * Create server and first route
    
    * INT-9 Merge master
    
    * Create server and first route
    
    * Create server and first route
    
    * Create server and first route

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,12 @@
-// const express = require('express');
+const express = require('express');
+const path = require('path');
 
-// create your server here
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+app.listen(9000);
