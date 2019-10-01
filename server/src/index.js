f220e9ec6ebb9b325632657324e20062c8416353
Dmitry Simakov <VAGNER2909@yandex.ru>
2019-10-01 14:15:53 +0300

    INT-21 Add db and insert function

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+
 const path = require('path');
 
 const app = express();
+
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://lol:kek@cluster0-gsf7q.gcp.mongodb.net/db?retryWrites=true&w=majority';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.info('MongoDB connected.'))
+  .catch((err) => console.info('MongoDB connection error: ', err));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -9,4 +18,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-app.listen(9000);
+const Port = process.env.Port || 3000;
+
+app.listen(Port, () => console.info('Server started.'));
