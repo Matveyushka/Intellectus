dea7cd5c4b5008404654adc3d9e315912c3d2225
STAJLb <RISEZID123>
2019-10-04 16:04:11 +0300

    Edit package.json (add body-parser lib), add answers route.

--- server/src/utils/session.js
+++ server/src/utils/session.js
@@ -2,12 +2,12 @@ const session = {};
 const saveSession = (token, questions) => {
   session[token] = {
     questions,
-    created_at: new Date().getTime(),
+    createdAt: new Date().getTime(),
   };
 };
 const getSession = token => session[token];
 
-const getTimeSession = token => new Date().getTime() - session[token].created_at;
+const getTimeSession = token => new Date().getTime() - session[token].createdAt;
 
 module.exports = {
   saveSession,
