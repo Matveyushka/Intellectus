f726f29d0a0f35b295add65cf2ca187000d2bd89
Slava Traktor <slavasobb@gmail.com>
2019-10-16 22:12:14 +0300

    Send feedback form

--- client/src/pages/ContactUs.tsx
+++ client/src/pages/ContactUs.tsx
@@ -1,39 +1,116 @@
 import * as React from 'react';
+import axios from 'axios';
+import useForm from 'react-hook-form';
 import { Footer } from '../components';
 
-export const ContactUs = (): React.ReactElement | null => (
-  <>
-    <main className="main-container">
-      <form className="contact-form">
-        <div className="input-field">
-          <label className="input-label" htmlFor="name">
-            <p className="label-title">Your name:</p>
-            <input className="input-text" type="text" id="name" name="name" />
-          </label>
-        </div>
-        <div className="input-field">
-          <label className="input-label" htmlFor="email">
-            <p className="label-title">Email:</p>
-            <input className="input-text" type="text" id="email" name="email" />
-          </label>
-        </div>
-        <div className="input-field">
-          <label className="input-label" htmlFor="text">
-            <p className="label-title">Feedback:</p>
-            <div className="input-with-area">
-              <input className="input-text" type="text" placeholder="Title" name="title" />
-              <textarea className="input-area" id="text" placeholder="Body" name="body" />
-              <div className="send-form">
-                <div className="area-count">0 / 300</div>
-                <button type="button" className="send-btn">
-                  <i className="plane-icon" />
-                </button>
+export const ContactUs = (): React.ReactElement | null => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const [textareaLength, setTextareaLength] = React.useState<number>(0);
+
+  const onTextareaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ): void => {
+    setTextareaLength(event.target.value.length);
+  };
+
+  const feedbackFormSubmit = (data: Record<string, string>): void => {
+    axios('/feedback', { method: 'post', data })
+      .then((res) => {
+        // eslint-disable-next-line no-console
+        console.log(res);
+      })
+      .catch((err: Error) => {
+        throw err;
+      });
+  };
+
+  return (
+    <>
+      <main className="main-container">
+        <form
+          className="contact-form"
+          onSubmit={handleSubmit(feedbackFormSubmit)}
+        >
+          <div className="input-field">
+            <label className="input-label" htmlFor="name">
+              <p className="label-title">
+                Your name:&nbsp;
+                <span className="label-error">
+                  {errors.name && 'Name required'}
+                </span>
+              </p>
+              <input
+                className="input-text"
+                type="text"
+                id="name"
+                name="name"
+                ref={register({ required: true })}
+              />
+            </label>
+          </div>
+          <div className="input-field">
+            <label className="input-label" htmlFor="email">
+              <p className="label-title">
+                Email:&nbsp;
+                <span className="label-error">
+                  {errors.email && errors.email.message}
+                </span>
+              </p>
+              <input
+                className="input-text"
+                type="text"
+                id="email"
+                name="email"
+                ref={register({
+                  required: 'Email required',
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: 'Email should looks like email',
+                  },
+                })}
+              />
+            </label>
+          </div>
+          <div className="input-field">
+            <label className="input-label" htmlFor="text">
+              <p className="label-title">
+                Feedback:&nbsp;
+                <span className="label-error">
+                  {errors.body && 'Body required'}
+                </span>
+              </p>
+              <div className="input-with-area">
+                <input
+                  className="input-text"
+                  type="text"
+                  placeholder="Title"
+                  name="title"
+                  ref={register}
+                />
+                <textarea
+                  className="input-area"
+                  id="text"
+                  placeholder="Body"
+                  name="body"
+                  onChange={onTextareaChange}
+                  ref={register({ required: true })}
+                />
+                <div className="send-form">
+                  <div className="area-count">
+                    {textareaLength}
+                    &nbsp;/ 300
+                  </div>
+                  <button type="submit" className="send-btn">
+                    <i className="plane-icon" />
+                  </button>
+                </div>
               </div>
-            </div>
-          </label>
-        </div>
-      </form>
-    </main>
-    <Footer />
-  </>
-);
+            </label>
+          </div>
+        </form>
+      </main>
+      <Footer />
+    </>
+  );
+};
