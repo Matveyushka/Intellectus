104596987446646c4b43a6604c8d2fc5b50fa519
Andrew <Andrew5282011@gmail.com>
2019-10-18 16:28:38 +0300

    Add .env instead of secure.json for successful deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const secure = require('../secure.json');
 const routes = require('./routes/index');
 const db = require('./db');
 
@@ -23,7 +23,7 @@ app.use('/questions', routes.questions);
 app.use('/answers', routes.answers);
 
 
-const { MONGO_ATLAS_LOGIN, MONGO_ATLAS_PASSWORD } = secure;
+const { MONGO_ATLAS_LOGIN, MONGO_ATLAS_PASSWORD } = process.env;
 const uri = `mongodb+srv://${MONGO_ATLAS_LOGIN}:${MONGO_ATLAS_PASSWORD}@cluster0-gsf7q.gcp.mongodb.net/db?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
