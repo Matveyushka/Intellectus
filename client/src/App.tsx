e79821fada0a04b2b956641b278cd6bddf6dbbed
Andrew <Andrew5282011@gmail.com>
2019-11-05 15:09:06 +0300

    INT-49 Show message on mobile landscape

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 import { MainLayout } from './MainLayout';
 
-export const App = (): React.ReactElement => (
-  <React.StrictMode>
-    <MainLayout />
-  </React.StrictMode>
-);
+export const App = (): React.ReactElement => {
+  const [isLandscape, setIsLandscape] = React.useState<boolean>(false);
+
+  React.useEffect((): void => {
+    window.addEventListener('orientationchange', () => {
+      const orientation = window.screen.orientation.type;
+
+      if (orientation === 'landscape-primary' || orientation === 'landscape-secondary') {
+        setIsLandscape(true);
+      } else {
+        setIsLandscape(false);
+      }
+    });
+  }, []);
+
+  if (isLandscape) {
+    return (
+      <div className="invalid-orientation">
+        <h1>Please rotate your device</h1>
+      </div>
+    );
+  }
+
+  return (
+    <React.StrictMode>
+      <MainLayout />
+    </React.StrictMode>
+  );
+};
