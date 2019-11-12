8ab3c49380c6c839eb1b804767b7d8165a310afd
Andrew <Andrew5282011@gmail.com>
2019-11-12 13:38:22 +0300

    INT-46 Add share feature

--- client/src/components/IntroView.tsx
+++ client/src/components/IntroView.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
+import mergeClassNames from 'classnames';
 import { useDispatch } from 'react-redux';
 import { Footer } from './Footer';
 import { Dispatch } from '../store';
 
 export const IntroView = (): React.ReactElement => {
   const dispatch: Dispatch = useDispatch();
+
+  const [isShareOpen, setIsShareOpen] = React.useState<boolean>(false);
+
+  const shareClassNames = mergeClassNames('share-buttons', {
+    hidden: !isShareOpen,
+  });
+
+  const shareSeparatorClassName = mergeClassNames('share-separator', {
+    hidden: !isShareOpen,
+  });
 
   const handlePlayButtonClick = (): void => {
     dispatch.main.getQuestions();
@@ -23,7 +34,37 @@ export const IntroView = (): React.ReactElement => {
         onClick={handlePlayButtonClick}
       />
       <Footer />
-      <button type="button" title="Share this site with your friends!" className="share-icon" />
+      <button
+        type="button"
+        title="Share this site with your friends!"
+        className="share-icon"
+        onClick={() => setIsShareOpen(!isShareOpen)}
+      />
+      <div className={shareSeparatorClassName} />
+      <div className={shareClassNames}>
+        <button
+          type="button"
+          data-sharer="twitter"
+          className="twitter"
+          data-title="Checkout Intellectus, challange your mind!"
+          data-hashtags="intellectus, iq, test"
+          data-url="https://intellectus-demo.herokuapp.com/"
+        />
+        <button
+          type="button"
+          className="facebook"
+          data-sharer="facebook"
+          data-hashtag="intellectus"
+          data-url="https://intellectus-demo.herokuapp.com/"
+        />
+        <button
+          type="button"
+          className="vk"
+          data-sharer="vk"
+          data-title="Checkout Intellectus, challange your mind!"
+          data-url="https://intellectus-demo.herokuapp.com/"
+        />
+      </div>
     </>
   );
 };
