b976f6a7e520eeb617416b63c974f2a5c702d003
Andrew <Andrew5282011@gmail.com>
2019-11-13 16:28:24 +0300

    INT-47 Fix screen on mobile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ export const App = (): React.ReactElement => {
   const [isLandscape, setIsLandscape] = React.useState<boolean>(false);
 
   React.useEffect((): void => {
+    if (navigator.userAgent.includes('Win')) return;
+
     window.addEventListener('orientationchange', () => {
       const orientation = window.screen.orientation.type;
 
@@ -16,17 +18,14 @@ export const App = (): React.ReactElement => {
     });
   }, []);
 
-  if (isLandscape) {
-    return (
-      <div className="invalid-orientation">
-        <h1>Please rotate your device</h1>
-      </div>
-    );
-  }
-
   return (
     <React.StrictMode>
       <MainLayout />
+      {isLandscape && (
+        <div className="invalid-orientation">
+          <h1>Please rotate your device</h1>
+        </div>
+      )}
     </React.StrictMode>
   );
 };
