9dd2bb8ce590cacb364b1d9c7d1801eea1929c44
mrZet <slavasobb@gmail.com>
2019-11-13 21:57:27 +0300

    INT-66 Result adaptive

--- client/src/components/TestResult.tsx
+++ client/src/components/TestResult.tsx
@@ -5,6 +5,7 @@ import { Stepper } from './Stepper';
 import { Dispatch, State } from '../store';
 import { MAIN_VIEW_TYPES } from '../constants';
 import { MainState } from '../pages/Main/model';
+import { formatTime } from '../components/TestView/helpers';
 
 export const TestResult = (): React.ReactElement => {
   const dispatch: Dispatch = useDispatch();
@@ -13,6 +14,7 @@ export const TestResult = (): React.ReactElement => {
     userAnswers,
     solutions = [],
     pointsDistribution = [],
+    resultTime
   } = useSelector<State, MainState>(state => state.main);
 
   const answers: boolean[] = userAnswers.map((item, index) => solutions[index] === item);
@@ -34,6 +36,7 @@ export const TestResult = (): React.ReactElement => {
         </h2>
         <StatisticChart rows={pointsDistribution} chosenRowIndex={solutionsCount} />
         <div className="test-hint">you are here</div>
+        <div className="test-time">{formatTime(resultTime ?? new Date(0, 0, 0))}</div>
       </div>
       <Stepper
         data={stepperData}
