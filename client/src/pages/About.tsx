56596e4e4f9bf1f0e8140e0327df919323e760d0
Cat-and <firan_toys@mail.ru>
2019-11-15 19:04:26 +0300

    INT-48 fixText2

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -12,25 +12,25 @@ export const About = ({ location }: AboutProps): React.ReactElement | null => (
     </div>
     <div className="about-content">
       <p>
-        Welcome to Intellectus project site. 
-        Here is the place where you can try to figure out how well your mind 
-        is able to solve logical tasks and perception problems.
+        {`Welcome to Intellectus project site. 
+        Here is the place where you can try to figure out how well your
+        mind is able to solve logical tasks and perception problems.`}
       </p>
       <p>
-        The feature of the Intellectus system is 
+        {`The feature of the Intellectus system is 
         using problem generating algorithms. 
         That means it is very little chance to face the same task you solved before. 
-        Each test session will be unique. 
+        Each test session will be unique.` }
       </p>
       <p>
-        Proposed testing is anonymous 
+        {`Proposed testing is anonymous 
         until you want to share your result with friends. 
         Intellectus collects information about number of people passed the test and their results - 
-        you can familiarize with our statistics - but not any personal data.
+        you can familiarize with our statistics - but not any personal data.`}
       </p>
       <p>
-        If you would like the site, share it to others. 
-        If you have a theme for conversation, write us an email.
+        {`If you would like the site, share it to others. 
+        If you have a theme for conversation, write us an email.`}
       </p>
     </div>
     <Footer />
