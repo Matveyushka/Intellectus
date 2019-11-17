4a5f83d5e7287865e410685c8a568d487bdba93b
Cat-and <firan_toys@mail.ru>
2019-11-17 14:18:43 +0300

    INT/71 fix Contact us

---
+++ client/src/pages/ContactUs/model.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { API } from '../../constants';
+import { Dispatch } from '../../store';
+import { DefaultContactData } from '../../components/forms/ContactUsForm';
+
+export interface ContactUsState {
+  isFinish: boolean;
+  error?: string;
+  oldData: DefaultContactData;
+}
+
+export const contactUs = {
+  state: {
+    isFinish: false,
+    error: undefined,
+    oldData: {},
+  } as ContactUsState,
+  reducers: {
+    setState: (state: ContactUsState, { isFinish, error, oldData }: ContactUsState) => ({
+      ...state,
+      isFinish,
+      error,
+      oldData,
+    }),
+    setIsSent: (state: ContactUsState, isFinish: boolean) => ({
+      ...state,
+      isFinish,
+    }),
+  },
+  effects: (dispatch: Dispatch) => ({
+    postFeedback: async (payload: DefaultContactData) => {
+      dispatch.loader.showLoader();
+      let error: string;
+
+      await axios.post(API.feedback, payload)
+        .catch((err: Error) => {
+          error = 'Something went wrong';
+
+          throw err;
+          // на самом деле это просто реджектит промис, а не крашит приложение
+        })
+        .finally(() => {
+          dispatch.contactUs.setState({
+            isFinish: true,
+            error,
+            oldData: payload,
+          });
+
+          dispatch.loader.hideLoader();
+        });
+    },
+  }),
+};
